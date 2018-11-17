61d0ac0af0b84919c1cfe8e3c2ebefa965c9471c
dango.yxm <yinxiaoming96@gmail.com>
2018-11-17 21:21:24 +0800

    pmd fix

--- web/src/test/java/us/betahouse/haetae/user/dal/service/UserRepoServiceTest.java
+++ web/src/test/java/us/betahouse/haetae/user/dal/service/UserRepoServiceTest.java
@@ -1,6 +1,5 @@
 package us.betahouse.haetae.user.dal.service;
 
-import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import us.betahouse.haetae.user.model.perm.UserBO;
 
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
