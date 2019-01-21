74a501e404a8031bf5218bc9f103872798137146
PengpengCao <995561635@qq.com>
2019-01-21 09:31:08 +0800

    创建活动的request封装写完，创建物资方法未写

---
+++ biz/impl/src/main/java/us/betahouse/haetae/serviceimpl/asset/service/impl/AssetServiceImpl.java
@@ -0,0 +1,23 @@
+/**
+ * betahouse.us
+ * CopyRight (c) 2012 - 2019
+ */
+package us.betahouse.haetae.serviceimpl.asset.service.impl;
+
+import us.betahouse.haetae.asset.dal.model.AssetDO;
+import us.betahouse.haetae.asset.request.AssetRequest;
+import us.betahouse.haetae.serviceimpl.asset.request.AssetManagerRequest;
+import us.betahouse.haetae.serviceimpl.asset.service.AssetService;
+import us.betahouse.haetae.serviceimpl.common.OperateContext;
+
+/**
+ * @author guofan.cp
+ * @version : AssetServiceImpl.java 2019/01/20 23:58 guofan.cp
+ */
+public class AssetServiceImpl implements AssetService {
+    @Override
+    public AssetDO create(AssetManagerRequest request, OperateContext context) {
+
+        return null;
+    }
+}
--- core/asset/src/main/java/us/betahouse/haetae/asset/dal/service/CreateAsset.java
+++ core/asset/src/main/java/us/betahouse/haetae/asset/dal/service/CreateAsset.java
@@ -1,5 +1,8 @@
 package us.betahouse.haetae.asset.dal.service;
 
+import us.betahouse.haetae.asset.dal.model.AssetDO;
+
 public interface CreateAsset {
+      AssetDO createAsset();
 
 }
