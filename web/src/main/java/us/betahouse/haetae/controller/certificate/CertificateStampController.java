7fb61b92f69e4f8c71177d3c3979b70974d29141
PengpengCao <995561635@qq.com>
2019-04-22 22:16:51 +0800

    1.dev2

---
+++ web/src/main/java/us/betahouse/haetae/controller/certificate/CertificateStampController.java
@@ -0,0 +1,12 @@
+/*
+ * betahouse.us
+ * CopyRight (c) 2012 - 2019
+ */
+package us.betahouse.haetae.controller.certificate;
+
+/**
+ * @author guofan.cp
+ * @version : CertificateStampController.java 2019/04/22 22:16 guofan.cp
+ */
+public class CertificateStampController {
+}
